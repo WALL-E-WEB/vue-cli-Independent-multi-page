cf2839d7bec48512d796fe71c35cbbf1f53d9f47
unknown <Wall.E>
2020-03-08 10:31:51 +0800

    独立多页面

---
+++ src/views/ui/page1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        page1111111111111111
+    </div>
+</template>
+<script>
+export default {
+    created(){
+        console.log('pageui');
+        
+    }
+}
+</script>
---
+++ src/views/ui/page2.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        page2222222222222
+    </div>
+</template>
